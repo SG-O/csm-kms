f99febfc93fc85b53c12cddad5b237df228aa3a0
SG-O <sg-o@live.de>
2025-09-20 18:17:14 +0200

    Minor fix

--- .github/script/build_sc_hsm.sh
+++ .github/script/build_sc_hsm.sh
@@ -10,13 +10,4 @@ autoreconf -fi
 ./configure --enable-ram --enable-libcrypto
 make
 
-COPY ./sc-hsm-embedded/src/pkcs11/.libs/libsc-hsm-pkcs11.so /usr/local/lib/libsc-hsm-pkcs11.so
-COPY ./sc-hsm-embedded/src/pkcs11/.libs/libsc-hsm-pkcs11.lai /usr/local/lib/libsc-hsm-pkcs11.la
-
-COPY ./sc-hsm-embedded/src/ramoverhttp/ram-client /usr/local/bin/ram-client
-COPY ./sc-hsm-embedded/src/ramoverhttp/ram-client /usr/local/bin/ram-client
-
-COPY ./sc-hsm-embedded/src/sc-hsm/sc-hsm-pkcs11.h /usr/local/include/sc-hsm/sc-hsm-pkcs11.h
-RUN mkdir -p /usr/local/etc/udev/rules.d
-COPY ./sc-hsm-embedded/etc/udev/rules.d/99-sc_hsm.rules /usr/local/etc/udev/rules.d/99-sc_hsm.rules
 
